060c0f8c734abd226a96cafb7b817d7e986af25d
Joseph <joseph.vitale@outlook.com>
2021-02-18 20:49:30 -0500

    adding BOM and code explanation and changed pot .cpp layout

--- src/Potentiometer.cpp
+++ src/Potentiometer.cpp
@@ -2,13 +2,13 @@
 #include <Potentiometer.h>
 
 //*****************************************Functions**************************//
+void Potentiometer::init_pot()
+{
+  pinMode(0, INPUT); // pin A0 mapped to an INPUT --> r_pot 
+} // end init_pot
+
 int Potentiometer::r_pot()
 {
   int potmap1 = map(pot1, 0, 1023, 1, 3); // map values 1-3 
   return (potmap1);
 } // end r_pot
-
-void Potentiometer::init_pot()
-{
-  pinMode(0, INPUT); // pin A0 mapped to an INPUT --> r_pot 
-} // end init_pot
