ad36282565bb99754baa61726d147f61489961fb
joseph <joseph.vitale@outlook.com>
2021-02-23 20:40:52 -0500

    Adding test Code

--- src/Potentiometer.cpp
+++ src/Potentiometer.cpp
@@ -15,5 +15,8 @@ int Potentiometer::r_pot()
 {
   int pot1 = analogRead(pot_pin_number);                 // private variable on pinnumber
   int potmap1 = map(pot1, 0, 1023, mapp_low, mapp_high); // map values
+
+  // Serial.println("Pot Vaule = ");
+  // Serial.println(potmap1);                             // print pot vaule in terminal 
   return (potmap1);
 } // end r_pot
