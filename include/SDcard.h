f75bdd919e3fe8889676134d11cd64e043f65efc
joseph <joseph.vitale@outlook.com>
2021-02-23 22:56:48 -0500

    added const char* to public

--- include/SDcard.h
+++ include/SDcard.h
@@ -5,8 +5,11 @@
 class SDcard
 {
 private:
-   // char Str_puzzle_name[15] ={"1015704.CSV" ,"561534.CSV", "1036958.CSV"};
+
+   
+   //{"1015704.CSV" ,"561534.CSV", "1036958.CSV"};
    // char mapp_arry [] [];
+    
 public:
     void int_SD();
     void print_SD_info();
@@ -14,6 +17,8 @@ public:
     void mapp();
     void printfiles();
     void close();
+    const char* str_puzzle_name[12] = {"1015704.CSV","561534.CSV","1036958.CSV"};
+    
 
     // figure out how to pass a spring to use close() and open() as a mobuler funtion
 }; // end Class SDcard
