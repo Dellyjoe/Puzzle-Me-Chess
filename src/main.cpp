890b82e2b222cee62bb84f56a201e2a5d59cab73
joseph <joseph.vitale@outlook.com>
2021-03-04 20:37:12 -0500

    Added Test Code

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@
 // puzzle a sound will play to tell the user has completed the puzzle.         //
 //*****************************************************************************//
 #include <Arduino.h>
+#include <Define.h>
 #include <Display.h>
 #include <SDcard.h>
 #include <Button.h>
@@ -39,135 +40,141 @@ void setup()
   SDcard0.print_directory();
   Serial.begin(9600);
   //******************************************Inputs***************************//
-  Button0.init_button(1);           // setting D1 to button
-  Potentiometer0.init_pot(0, 1, 3); // setting A0 to pot
-  Switch0.init_switch(0);           // seeting D0 to switch
+  Button0.init_button(1);                                        // setting D1 to button
+  Potentiometer0.init_pot(0, 1, 3);                              // setting A0 to pot
+  Switch0.init_switch(0);                                        // seeting D0 to switch
   Mulitiplexer0.init_muxs_input(36, 37, 38, 39, 23, 22, 21, 20); // pins will have to be moved
   //******************************************Outputs**************************//
-  Buzzer0.init_buzzer(15);           // setting A1 to buzzer
+  Buzzer0.init_buzzer(15);                    // setting A1 to buzzer
   Mulitiplexer0.init_muxs_output(35, 34, 33); // setting output channels s1 = a, s2 = b, s3 = c
-  Mulitiplexer0.init_enable(16); 
+  Mulitiplexer0.init_enable(16);
 } // end setup
 
 void loop()
 {
   //***************************************Test Code***************************//
-  // Potentiometer0.r_pot();
-  // Button0.r_button();
-  // Switch0.r_switch();
-  // while (Button0.r_button() == HIGH)
-  // {
-  //   Buzzer0.write_buzzer();
-  // }
-
-  // Buzzer0.turn_off_buzzer();
-  // delay(1000);//--> to allow for button press
-  
-  Mulitiplexer0.channel_select(0, 0, 0); 
-  Mulitiplexer0.set_enable(0);
-
-   while (Button0.r_button() == HIGH)
+  if (test_code == true)
   {
-  //Mulitiplexer0.r_mux_channel(23);
-  Mulitiplexer0.r_mux_channel();
-  delay(1000);
-  // Serial.println("MuxA pin is");
-  // Serial.println(MuxA);
-  //delay(5000);
-  } // test for mux circuit 
+    Potentiometer0.r_pot();
+    Button0.r_button();
+    Switch0.r_switch();
+    Mulitiplexer0.set_enable(0);
 
-  delay(1000); //--> to allow for button press
+  //  while (Button0.r_button() == HIGH)
+  //   {
+  //     Buzzer0.write_buzzer();
+  //     delay(1000);
+  //     Buzzer0.turn_off_buzzer();
+  //     delay(1000);
+  //   }
+  //   Buzzer0.turn_off_buzzer();
+  //   delay(1000);//--> to allow for button press
+
+    while (Button0.r_button() == HIGH)
+    {
+      Serial.println("Block A1");
+       Mulitiplexer0.channel_select(0, 0, 0);
+        Mulitiplexer0.r_mux_channel();
+        delay(1);
+        Serial.println("Block A2");
+        Mulitiplexer0.channel_select(1, 0, 0);
+        Mulitiplexer0.r_mux_channel();
+      delay(4000);
+    } // end test for mux circuit
+    delay(1000); //--> to allow for button press
+  }
+
   //***************************************Start of Code***********************//
 
-
-  while (Button0.r_button() == HIGH)
+  if (test_code == false)
   {
+    while (Button0.r_button() == HIGH)
+    {
+      Display0.clear();
+      Display0.print_select_puzzle(60, 30, Potentiometer0.r_pot());
+      Display0.draw();
+    }
+
+    delay(1000); //--> to allow for button press
+
     Display0.clear();
-    Display0.print_select_puzzle(60, 30, Potentiometer0.r_pot());
+    Display0.print_user_puzzle(58, 43, Potentiometer0.r_pot());
     Display0.draw();
-  }
 
-  delay(1000); //--> to allow for button press
+    switch (Potentiometer0.r_pot())
+    {
+    case 1:
+      Serial.println("Opening File 1015704.CSV"); // prints index 0 in const char array
+      Serial.println("___________________");      // line breakup for readability
+      SDcard0.open_file(0);                       // Open File indexer 0
+      Serial.println("___________________");
+      break;
+    case 2:
+      Serial.println("Opening File 561534.CSV");
+      Serial.println("___________________");
+      SDcard0.open_file(1);
+      Serial.println("___________________");
+      break;
+    case 3:
+      Serial.println("Opening File 1036958.CSV");
+      Serial.println("___________________");
+      SDcard0.open_file(2);
+      Serial.println("___________________");
+      break;
+    }
 
-  Display0.clear();
-  Display0.print_user_puzzle(58, 43, Potentiometer0.r_pot());
-  Display0.draw();
+    while ((Button0.r_button() == HIGH) & (Potentiometer0.r_pot() == 1)) // to set up Puzzle #1015704
+    {
 
-  switch (Potentiometer0.r_pot())
-  {
-  case 1:
-  
-    Serial.println("Opening File 1015704.CSV"); // prints index 0 in const char array
-    Serial.println("___________________");      // line breakup for readability
-    SDcard0.open_file(0);                       // Open File indexer 0
-    Serial.println("___________________"); 
-    break;
-  case 2:
-    Serial.println("Opening File 561534.CSV");
-    Serial.println("___________________");
-    SDcard0.open_file(1);
-    Serial.println("___________________");
-    break;
-  case 3:
-    Serial.println("Opening File 1036958.CSV");
-    Serial.println("___________________"); 
-    SDcard0.open_file(2);
-    Serial.println("___________________");
-    break;
-  }
+      LED0.charlie(LED0.LEDA1);
+      delay(1);
+      LED0.charlie(LED0.LEDA2);
+      delay(1);
+      LED0.charlie(LED0.LEDA3);
+      delay(1);
+      // LED0.charlie(LED0.LEDB1);
+      // delay(1);
+      LED0.charlie(LED0.LEDB2);
+      delay(1);
+      // LED0.charlie(LED0.LEDB3);
+      // delay(1);
+    }
 
-  while ((Button0.r_button() == HIGH) & (Potentiometer0.r_pot() ==1)) // to set up Puzzle #1015704
-  {
-    
-    LED0.charlie(LED0.LEDA1);
-    delay(1);
-    LED0.charlie(LED0.LEDA2);
-    delay(1);
-    LED0.charlie(LED0.LEDA3);
-    delay(1);
-    // LED0.charlie(LED0.LEDB1);
-    // delay(1);
-    LED0.charlie(LED0.LEDB2);
-    delay(1);
-    // LED0.charlie(LED0.LEDB3);
-    // delay(1);
-  }
+    while ((Button0.r_button() == HIGH) & (Potentiometer0.r_pot() == 2)) // to set up Puzzle #561534
+    {
+      LED0.charlie(LED0.LEDA1);
+      delay(1);
+      LED0.charlie(LED0.LEDA2);
+      delay(1);
+      // LED0.charlie(LED0.LEDB1);
+      // delay(1);
+      // LED0.charlie(LED0.LEDB1);
+      // delay(1);
+      LED0.charlie(LED0.LEDB2);
+      delay(1);
+      // LED0.charlie(LED0.LEDB3);
+      // delay(1);
+    }
 
+    while ((Button0.r_button() == HIGH) & (Potentiometer0.r_pot() == 3)) // to set up Puzzle #1036958
+    {
+      LED0.charlie(LED0.LEDA1);
+      delay(1);
+      // LED0.charlie(LED0.LEDA2);
+      // delay(1);
+      // LED0.charlie(LED0.LEDB1);
+      // delay(1);
+      // LED0.charlie(LED0.LEDB1);
+      // delay(1);
+      // LED0.charlie(LED0.LEDB2);
+      // delay(1);
+      // LED0.charlie(LED0.LEDB3);
+      // delay(1);
+    }
 
-  while ((Button0.r_button() == HIGH) & (Potentiometer0.r_pot() ==2)) // to set up Puzzle #561534
-  {
-    LED0.charlie(LED0.LEDA1);
-    delay(1);
-    LED0.charlie(LED0.LEDA2);
-    delay(1);
-     // LED0.charlie(LED0.LEDB1);
-    // delay(1);
-    // LED0.charlie(LED0.LEDB1);
-    // delay(1);
-    LED0.charlie(LED0.LEDB2);
-    delay(1);
-    // LED0.charlie(LED0.LEDB3);
-    // delay(1);
-  }
-
-  while ((Button0.r_button() == HIGH) & (Potentiometer0.r_pot() ==3)) // to set up Puzzle #1036958
-  {
-    LED0.charlie(LED0.LEDA1);
-    delay(1);
-    // LED0.charlie(LED0.LEDA2);
-    // delay(1);
-     // LED0.charlie(LED0.LEDB1);
-    // delay(1);
-    // LED0.charlie(LED0.LEDB1);
-    // delay(1);
-    // LED0.charlie(LED0.LEDB2);
-    // delay(1);
-    // LED0.charlie(LED0.LEDB3);
-    // delay(1);
-  }
-
-  LED0.charlie(LED0.LEDclear); // clears all LEDs
-  delay(1000); // allow for button press
+    LED0.charlie(LED0.LEDclear); // clears all LEDs
+    delay(1000);                 // allow for button press
+  } // end  if (test_code == false)
 
   // look into making a grid for the board that will use your Muxltiplexers
   // to turn on or off leds and also read in voltage levels
