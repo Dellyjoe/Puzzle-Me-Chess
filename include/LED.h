373724db08b3ea000d8e2e9dc2f8ee8e3b9b321f
joseph <joseph.vitale@outlook.com>
2021-03-14 18:43:30 -0400

    Removed CharliePlexing, added own 8x8 LED Grid

--- include/LED.h
+++ include/LED.h
@@ -4,17 +4,15 @@ class LED
 private:
     int index = 0; 
 public:
-    void turn_on();
-    void turn_off();
     void init_LED();
-    void charlie(int LEDarry[]);
+    void LED_on_off(int LEDarry[]);
 
-//******************************************LEDS***************************//
-    int LEDA1[3] = {HIGH, LOW ,-1};
-    int LEDA2[3] = {LOW, HIGH, -1};
-    int LEDA3[3] = {LOW, -1, HIGH};
-    int LEDB1[3] = {HIGH, -1, LOW};
-    int LEDB2[3] = {-1, HIGH, LOW};
-    int LEDB3[3] = {-1, LOW, HIGH};
-    int LEDclear[3] = {-1,-1, -1}; //clears all LEDS
+//******************************************LEDS***************************/
+    int LED1[7] = {OUTPUT, INPUT, OUTPUT, INPUT, INPUT, 1 , 3};
+    int LED2[7] = {INPUT, OUTPUT, OUTPUT, INPUT, INPUT, 2 , 3};
+    int LED3[7] = {OUTPUT, INPUT, INPUT, OUTPUT, INPUT, 1 , 4};
+    int LED4[7] = {INPUT, OUTPUT, INPUT, OUTPUT, INPUT, 2 , 4};
+    int LED5[7] = {OUTPUT, INPUT, INPUT, INPUT, OUTPUT, 1, 5};
+    int LED6[7] = {INPUT, OUTPUT, INPUT, INPUT, OUTPUT, 2 , 5};
+    int LEDclear[7] = {INPUT, INPUT, INPUT, INPUT, INPUT, 0 , 0};
 }; // end Class LED
--- src/LED.cpp
+++ src/LED.cpp
@@ -2,58 +2,81 @@
 #include <LED.h>
 
 //******************************************Declare****************************//
-#define CHARLIE_24 24 // pin number
-#define CHARLIE_25 25
-#define CHARLIE_26 26
-#define CHARLIE_27 27
-#define CHARLIE_28 28
-#define CHARLIE_29 29
-#define CHARLIE_30 30
-#define CHARLIE_31 31
-#define CHARLIE_32 32
+// #define CHARLIE_24 24 // pin number
+// #define CHARLIE_25 25
+// #define CHARLIE_26 26
+// #define CHARLIE_27 27
+// #define CHARLIE_28 28
+// #define CHARLIE_29 29
+// #define CHARLIE_30 30
+// #define CHARLIE_31 31
+// #define CHARLIE_32 32
+
+#define LED_Channel_1 1 // pin number
+#define LED_Channel_2 2
+#define LED_Channel_3 3
+#define LED_Channel_4 4
+#define LED_Channel_5 5
+#define LED_Channel_6 6
 
 //******************************************Setup******************************//
 void LED::init_LED(){
 
   //******************************************Inputs***************************//
-  pinMode(CHARLIE_24, OUTPUT);
-  pinMode(CHARLIE_25, OUTPUT);
-  pinMode(CHARLIE_26, OUTPUT);
-  pinMode(CHARLIE_27, OUTPUT);
-  pinMode(CHARLIE_28, OUTPUT);
-  pinMode(CHARLIE_29, OUTPUT);
-  pinMode(CHARLIE_30, OUTPUT);
-  pinMode(CHARLIE_31, OUTPUT);
-  pinMode(CHARLIE_32, OUTPUT);
-  } // end init_LED
+  pinMode(LED_Channel_1, OUTPUT);
+  pinMode(LED_Channel_2, OUTPUT);
+  pinMode(LED_Channel_3, OUTPUT);
+  pinMode(LED_Channel_4, OUTPUT);
+  pinMode(LED_Channel_5, OUTPUT);
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
 
-void LED::charlie(int LEDarry[] ) // ,int d, int e, int f, int g, int H,
+} // end init_LED
+
+void LED::LED_on_off(int LEDarry[])
 {
-  index = LEDarry[0]; // Starts at A
 
-  if(index < 0){
-    pinMode(CHARLIE_24, INPUT);
-  } else {
-    pinMode(CHARLIE_24, OUTPUT);
-    digitalWrite(CHARLIE_24, index);
-  }
+  pinMode(LED_Channel_1, LEDarry[0]);
+  pinMode(LED_Channel_2, LEDarry[1]);
+  pinMode(LED_Channel_3, LEDarry[2]);
+  pinMode(LED_Channel_4, LEDarry[3]);
+  pinMode(LED_Channel_5, LEDarry[4]);
+  digitalWrite(LEDarry[5], HIGH);
+  digitalWrite(LEDarry[6], LOW);
+  delay(1);
 
-  index = LEDarry[1];
+} // end LED_on
 
-   if(index < 0){
-    pinMode(CHARLIE_25, INPUT);
-  } else {
-    pinMode(CHARLIE_25, OUTPUT);
-    digitalWrite(CHARLIE_25, index);
-  }
+// void LED::charlie(int LEDarry[] ) // ,int d, int e, int f, int g, int H,
+// {
+//   index = LEDarry[0]; // Starts at A
 
-  index = LEDarry[2];
+//   if(index < 0){
+//     pinMode(CHARLIE_24, INPUT);
+//   } else {
+//     pinMode(CHARLIE_24, OUTPUT);
+//     digitalWrite(CHARLIE_24, index);
+//   }
 
-   if(index < 0){
-    pinMode(CHARLIE_26, INPUT);
-  } else {
-    pinMode(CHARLIE_26, OUTPUT);
-    digitalWrite(CHARLIE_26, index);
-  }
-  delay(1); // adds 1ms delay to allow for caps to discharge
-} // end class charlie
+//   index = LEDarry[1];
+
+//    if(index < 0){
+//     pinMode(CHARLIE_25, INPUT);
+//   } else {
+//     pinMode(CHARLIE_25, OUTPUT);
+//     digitalWrite(CHARLIE_25, index);
+//   }
+
+//   index = LEDarry[2];
+
+//    if(index < 0){
+//     pinMode(CHARLIE_26, INPUT);
+//   } else {
+//     pinMode(CHARLIE_26, OUTPUT);
+//     digitalWrite(CHARLIE_26, index);
+//   }
+//   delay(1); // adds 1ms delay to allow for caps to discharge
+// } // end class charlie
