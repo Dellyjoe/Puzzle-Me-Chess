ecdffa3ca7153b2f0a6beebb048601cdc7f5787c
Joseph <joseph.vitale@outlook.com>
2021-03-21 18:12:23 -0400

    Adding A1-A8 LED Arrarys

--- src/LED.cpp
+++ src/LED.cpp
@@ -2,22 +2,24 @@
 #include <LED.h>
 
 //******************************************Declare****************************//
-// #define CHARLIE_24 24 // pin number
-// #define CHARLIE_25 25
-// #define CHARLIE_26 26
-// #define CHARLIE_27 27
-// #define CHARLIE_28 28
-// #define CHARLIE_29 29
-// #define CHARLIE_30 30
-// #define CHARLIE_31 31
-// #define CHARLIE_32 32
-
+//******************Row****************// 
 #define LED_Channel_1 1 // pin number
 #define LED_Channel_2 2
 #define LED_Channel_3 3
 #define LED_Channel_4 4
 #define LED_Channel_5 5
 #define LED_Channel_6 6
+#define LED_Channel_7 7
+#define LED_Channel_8 8
+//******************Col****************//
+#define LED_Channel_9 9 // pin number
+#define LED_Channel_10 10
+#define LED_Channel_11 11
+#define LED_Channel_12 12
+#define LED_Channel_24 24
+#define LED_Channel_25 25
+#define LED_Channel_26 26
+#define LED_Channel_27 27
 
 //******************************************Setup******************************//
 void LED::init_LED(){
@@ -26,11 +28,34 @@ void LED::init_LED(){
   pinMode(LED_Channel_3, OUTPUT);
   pinMode(LED_Channel_4, OUTPUT);
   pinMode(LED_Channel_5, OUTPUT);
-  digitalWrite(1, HIGH);
-  digitalWrite(2, HIGH);
-  digitalWrite(3, HIGH);
-  digitalWrite(4, HIGH);
-  digitalWrite(5, HIGH);
+  pinMode(LED_Channel_6, OUTPUT);
+  pinMode(LED_Channel_7, OUTPUT);
+  pinMode(LED_Channel_8, OUTPUT);
+  pinMode(LED_Channel_9, OUTPUT);
+  pinMode(LED_Channel_10, OUTPUT);
+  pinMode(LED_Channel_11, OUTPUT);
+  pinMode(LED_Channel_12, OUTPUT);
+  pinMode(LED_Channel_24, OUTPUT);
+  pinMode(LED_Channel_25, OUTPUT);
+  pinMode(LED_Channel_26, OUTPUT);
+  pinMode(LED_Channel_27, OUTPUT);
+
+  digitalWrite(LED_Channel_1, HIGH);
+  digitalWrite(LED_Channel_2, HIGH);
+  digitalWrite(LED_Channel_3, HIGH);
+  digitalWrite(LED_Channel_4, HIGH);
+  digitalWrite(LED_Channel_5, HIGH);
+  digitalWrite(LED_Channel_6, HIGH);
+  digitalWrite(LED_Channel_7, HIGH);
+  digitalWrite(LED_Channel_8, HIGH);
+  digitalWrite(LED_Channel_9, HIGH);
+  digitalWrite(LED_Channel_10, HIGH);
+  digitalWrite(LED_Channel_11, HIGH);
+  digitalWrite(LED_Channel_12, HIGH);
+  digitalWrite(LED_Channel_24, HIGH);
+  digitalWrite(LED_Channel_25, HIGH);
+  digitalWrite(LED_Channel_26, HIGH);
+  digitalWrite(LED_Channel_27, HIGH);
 
 } // end init_LED
 
@@ -42,8 +67,20 @@ void LED::LED_on_off(int LEDarry[])
   pinMode(LED_Channel_3, LEDarry[2]);
   pinMode(LED_Channel_4, LEDarry[3]);
   pinMode(LED_Channel_5, LEDarry[4]);
-  digitalWrite(LEDarry[5], HIGH);
-  digitalWrite(LEDarry[6], LOW);
+  pinMode(LED_Channel_6, LEDarry[5]);
+  pinMode(LED_Channel_7, LEDarry[6]);
+  pinMode(LED_Channel_8, LEDarry[7]);
+  pinMode(LED_Channel_9, LEDarry[8]);
+  pinMode(LED_Channel_10, LEDarry[9]);
+  pinMode(LED_Channel_11, LEDarry[10]); 
+  pinMode(LED_Channel_12, LEDarry[11]);
+  pinMode(LED_Channel_24, LEDarry[12]);
+  pinMode(LED_Channel_25, LEDarry[13]);
+  pinMode(LED_Channel_26, LEDarry[14]);
+  pinMode(LED_Channel_27, LEDarry[15]);
+  
+  digitalWrite(LEDarry[16], HIGH);
+  digitalWrite(LEDarry[17], LOW);
   delay(1);
 
 } // end LED_on
