6098ce37248efae7ef9e377dcbf94f1a0036ba79
Joseph <joseph.vitale@outlook.com>
2021-03-27 13:11:51 -0400

    Changed init_mux_input lines

--- src/main.cpp
+++ src/main.cpp
@@ -44,7 +44,7 @@ void setup()
   Button0.init_button(22);                                       // setting D1 to button
   Potentiometer0.init_pot(23, 1, 3);                             // setting A9 to pot/Enabling pot
   Switch0.init_switch(21);                                       // seeting D0 to switch
-  Mulitiplexer0.init_muxs_input(37, 38, 39, 39, 13, 14, 15, 16); 
+  Mulitiplexer0.init_muxs_input(37, 38, 39, 13, 14, 15, 16, 17); 
   //******************************************Outputs**************************//
   Buzzer0.init_buzzer(21);                    // setting A1 to buzzer
   Mulitiplexer0.init_muxs_output(35, 34, 33); // setting output channels s1 = a, s2 = b, s3 = c
